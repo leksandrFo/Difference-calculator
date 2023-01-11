d5517ba9da35f16962c0b8ea6e22688c97108b90
leksandrFo <leksandr.fo@gmail.com>
2023-01-11 22:11:22 +0300

    added name, helpOption,  option, arguments and action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
 program.parse();
-
-

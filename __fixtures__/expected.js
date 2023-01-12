34e36e3af6dcf931afc7c2bf72afc258978562e3
leksandrFo <leksandr.fo@gmail.com>
2023-01-12 23:47:14 +0300

    added expected.js

---
+++ __fixtures__/expected.js
@@ -0,0 +1,5 @@
+export default () => {
+  const string =
+  '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+  return string;
+};

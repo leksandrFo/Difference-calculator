02d621585df68faafd9a5ba6589a991a20302ff6
leksandrFo <leksandr.fo@gmail.com>
2023-01-12 23:48:05 +0300

    added tests folder

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { genDiff } from '../src/index.js';
+import expected from '../__fixtures__/expected.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('genDiff', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected());
+});

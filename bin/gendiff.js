516a9335d7d76ddbdb582cae1fcbdf5e0f0116c5
leksandrFo <leksandr.fo@gmail.com>
2023-01-18 23:47:03 +0300

    added options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,10 +7,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+    const options = program.opts();
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
   });
 

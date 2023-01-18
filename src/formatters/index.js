7c8ca8b2d5ce874e77a90a70509661b60faed4f7
leksandrFo <leksandr.fo@gmail.com>
2023-01-18 23:52:54 +0300

    added formatters folder with index.js and stylish.js

---
+++ src/formatters/index.js
@@ -0,0 +1,12 @@
+import stylish from './stylish.js';
+
+export default (data, format) => {
+  switch (format) {
+    case 'stylish':
+      return stylish(data);
+    case 'json':
+      return JSON.stringify(data);
+    default:
+      throw new Error(`Unknown format: '${format}'!`);
+  }
+};
---
+++ src/formatters/stylish.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+const insertFullIndent = (depth, indent = ' ', indentCount = 4) => _.repeat(indent, indentCount * depth);
+const insertShortIndent = (depth, indent = ' ', indentCount = 4) => _.repeat(indent, (indentCount * depth) - 2);
+
+const getString = (obj, depth) => {
+  if (!_.isPlainObject(obj)) {
+    return obj;
+  }
+  const result = Object.entries(obj).map(([key, value]) => `${insertFullIndent(depth + 1)}${key}: ${getString(value, depth + 1)}`);
+  return `{\n${result.join('\n')}\n${insertFullIndent(depth)}}`;
+};
+
+const insertString = (key, value, depth, symbol = ' ') => `${insertShortIndent(depth)}${symbol} ${key}: ${getString(value, depth)}`;
+
+export default (tree) => {
+  const iter = (node, depth) => node.map(({ key, value, type }) => {
+    switch (type) {
+      case 'added':
+        return insertString(key, value, depth, '+');
+      case 'removed':
+        return insertString(key, value, depth, '-');
+      case 'unmodified':
+        return insertString(key, value, depth);
+      case 'modified':
+        return `${insertString(key, value[0], depth, '-')}\n${insertString(key, value[1], depth, '+')}`;
+      case 'nested':
+        return `${insertFullIndent(depth)}${key}: {\n${iter(value, depth + 1).join('\n')}\n${insertFullIndent(depth)}}`;
+      default:
+        throw new Error(`Unknown property type: '${type}'!`);
+    }
+  });
+  const result = iter(tree, 1);
+  return `{\n${result.join('\n')}\n}`;
+};

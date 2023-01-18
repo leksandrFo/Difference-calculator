af04a58b442a6ddf754c5160e4798098e2edcdee
leksandrFo <leksandr.fo@gmail.com>
2023-01-18 23:46:15 +0300

    added and modified tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,22 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { genDiff } from '../src/index.js';
+import { getExtension, genDiff } from '../src/index.js';
 import expected from '../__fixtures__/expected.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('genDiff', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected());
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.yml'))).toBe(expected());
+test('getExtension', () => {
+  expect(getExtension(getFixturePath('file1.json'))).toEqual('.json');
 });
 
 test('Extension error', () => {
   expect(() => genDiff(getFixturePath('file1.json'), getFixturePath('expected.js'))).toThrow('The format .js is not supported');
+});
+
+test('genDiff', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(expected);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.yml'))).toEqual(expected);
 });

6540d5cc3aa17051a0b0b4b5a2532f9a2f9dfb34
leksandrFo <leksandr.fo@gmail.com>
2023-01-21 12:56:37 +0300

    modified expected.js for new tests

--- __fixtures__/expected.js
+++ __fixtures__/expected.js
@@ -1,4 +1,4 @@
-const expected = `{
+const stylishExpected = `{
     common: {
       + follow: false
         setting1: Value 1
@@ -42,5 +42,26 @@ const expected = `{
         fee: 100500
     }
 }`;
+
+const plainExpected = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
+
+const expected = (formatName) => {
+  if (formatName === 'stylish') {
+    return stylishExpected;
+  } if (formatName === 'plain') {
+    return plainExpected;
+  }
+  return stylishExpected;
+};
 
 export default expected;
